a5de3566922ff8b82eb6c2902b3b5bae8c2b96f3
Khoa Tran <khoa.d.tran97@gmail.com>
2022-01-16 22:34:31 -0800

    added sorting capability

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,15 +1,55 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Feed.css";
 import ItemEntry from "./ItemEntry";
 import testdata from "./testdata.json";
+import sortByDate from "./../utils/FeedSort";
+import {
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  FormControl,
+  Radio,
+} from "@mui/material";
 
-function Feed() {
+const Feed = () => {
   const bottomRef = useRef();
+  const [items, setItems] = useState(
+    testdata.items.map((item) => ({
+      itemName: item.itemName,
+      expiDate: item.expiDate,
+    }))
+  );
 
-  const payload = testdata;
-  const listItems = payload.items.map((item) => (
-    <p key={item.itemName}>{item.itemName + " : " + item.expiDate}</p>
-  ));
+  const sortByName = (objs) => {
+    return [...objs].sort((a, b) => a.itemName.localeCompare(b.itemName));
+  };
+
+  const sortByDate = (objs) => {
+    const custom_sort = (a, b) => {
+      return new Date(a.expiDate).getTime() - new Date(b.expiDate).getTime();
+    };
+    return [...objs].sort(custom_sort);
+  };
+
+  const sortByNameHandler = () => {
+    let sorted = sortByName(items);
+    setItems(sorted);
+  };
+
+  const sortByDateHandler = () => {
+    let sorted = sortByDate(items);
+    setItems(sorted);
+  };
+
+  // const payload = testdata;
+  // const listItems = payload.items.map((item) => (
+  //   <p key={item.itemName}>{item.itemName + " : " + item.expiDate}</p>
+  // ));
+
+  const handleNewItemEntry = (newItem) => {
+    let sorted = sortByDate([...items, newItem]);
+    setItems(sorted);
+  };
 
   const scrollToBottom = () => {
     bottomRef.current.scrollIntoView({
@@ -18,19 +58,48 @@ function Feed() {
     });
   };
 
+  useEffect(() => {
+    sortByDateHandler();
+  }, []);
+
   return (
     <div className="feed">
       <button type="button" onClick={scrollToBottom}>
         Scroll To Bottom
       </button>
-      <h1>Item : Expiration</h1>
+      <h3>Item : Expiration</h3>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Sort By</FormLabel>
+        <RadioGroup row defaultValue="date" name="radio-buttons-group">
+          <FormControlLabel
+            onChange={sortByDateHandler}
+            value="date"
+            control={<Radio />}
+            label="Date"
+          />
+          <FormControlLabel
+            onChange={sortByNameHandler}
+            value="item"
+            control={<Radio />}
+            label="Item"
+          />
+        </RadioGroup>
+      </FormControl>
       <div>
-        <p>{listItems}</p>
-        <div ref={bottomRef} className="list-bottom"></div>
+        {items.map((item) => (
+          <div className="food-date-list" ref={bottomRef}>
+            <div className="item">{item.itemName}</div>
+            <div className="expi-date">
+              {new Date(item.expiDate).toDateString()}
+            </div>
+          </div>
+        ))}
+        {/* <p>{listItems}</p>
+        <div ref={bottomRef} className="list-bottom"></div> */}
       </div>
-      <ItemEntry />
+      <ItemEntry onEnteringNewItem={handleNewItemEntry} />
     </div>
   );
-}
+};
 
 export default Feed;
--- src/components/ItemEntry.js
+++ src/components/ItemEntry.js
@@ -1,16 +1,103 @@
 import React, { useState } from "react";
-import "./ItemEntry.css";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
-function ItemEntry() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
+
+import "./ItemEntry.css";
+import TextField from "@mui/material/TextField";
+
+const ItemEntry = (props) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [itemName, setItemName] = useState("");
+  const handleDateChange = (date) => {
+    console.log(date);
+    setSelectedDate(date);
+  };
+
+  const handleSubmit = (event) => {
+    props.onEnteringNewItem({
+      itemName: itemName,
+      expiDate: selectedDate.toDateString(),
+    });
+    handleClose();
+    setItemName("");
+    event.preventDefault();
+  };
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
     <div className="item-entry">
       <button className="button">Scan Item</button>
-      <button className="button">Manual Entry</button>
+
+      <button onClick={handleOpen} className="button">
+        Manual Entry
+      </button>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form onSubmit={handleSubmit}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Manual Entry
+            </Typography>
+
+            <TextField
+              onChange={(event) => setItemName(event.target.value.trim())}
+              id="standard-basic"
+              label="Item Name"
+              variant="standard"
+            />
+
+            <p>Expiration Date</p>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                clearable
+                value={selectedDate}
+                placeholder="01/01/2001"
+                onChange={(date) => handleDateChange(date)}
+                minDate={new Date()}
+                format="MM/dd/yyyy"
+              />
+            </MuiPickersUtilsProvider>
+            <Button
+              variant="contained"
+              type="submit"
+              disabled={itemName === "" ? true : false}
+            >
+              Submit
+            </Button>
+          </form>
+        </Box>
+      </Modal>
     </div>
   );
-}
+};
 
 export default ItemEntry;
